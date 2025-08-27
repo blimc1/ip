e6d498b6cb17b570587042f06f9ada45f43f611a
brien <brienlim21@gmail.com>
2025-08-28 01:47:36 +0800

    Added ability to mark tasks as done and not done, using a separate class for tasks

--- src/main/java/Doge.java
+++ src/main/java/Doge.java
@@ -18,26 +18,44 @@ public class Doge {
         Scanner scanner = new Scanner(System.in);
 
         //array to store text input by the user
-        String[] textList = new String[100];
-        int numberOfTexts = 0;
+        Task[] taskList = new Task[100];
+        int numberOfTasks = 0;
 
         while(true) {
             String input = scanner.nextLine(); //reads input
+            Task t = new Task(input);
 
-            if (input.equals("bye")) {
+            if (input.equals("bye")) { //handles exit
                 System.out.println("____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n");
                 break;
             } else if (input.equals("list")) {
-                System.out.println("____________________________________________________________\n");
-                for (int i = 0; i < numberOfTexts; i++) {
-                    System.out.println((i + 1) + ". " + textList[i]);
+                System.out.println("____________________________________________________________\n" +
+                        "Here are the tasks in your list:");
+                for (int i = 0; i < numberOfTasks; i++) {
+                    System.out.println((i + 1) + "." + taskList[i].listTasks());
                 }
                 System.out.println("____________________________________________________________\n");
-            } else {
-                textList[numberOfTexts] = input;
-                numberOfTexts++;
+            } else if (input.startsWith("mark ")) { //mark as done
+                String[] inputParts  = input.split(" ");
+                int taskNumber = Integer.parseInt(inputParts[1]) - 1;
+                taskList[taskNumber].markAsDone();
+                System.out.println("____________________________________________________________\n" +
+                        "Nice! I've marked this task as done: \n" +
+                        taskList[taskNumber].listTasks() +
+                        "\n____________________________________________________________\n");
+            } else if (input.startsWith("unmark ")) { //unmark done task
+                String[] inputParts  = input.split(" ");
+                int taskNumber = Integer.parseInt(inputParts[1]) - 1; //converts string to int
+                taskList[taskNumber].markAsNotDone();
+                System.out.println("____________________________________________________________\n" +
+                        "OK, I've marked this task as not done yet: \n" +
+                        taskList[taskNumber].listTasks() +
+                        "\n____________________________________________________________\n");
+            } else { //echo task
+                taskList[numberOfTasks] = t;
+                numberOfTasks++;
                 System.out.println("____________________________________________________________\n" +
                         "added: " + input +
                         "\n____________________________________________________________\n");
