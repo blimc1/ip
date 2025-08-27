46bb0e24eec78450f4946191cd805b86696c1b6a
brien <brienlim21@gmail.com>
2025-08-27 15:28:11 +0800

    Changed name to Doge

---
+++ src/main/java/Doge.java
@@ -0,0 +1,11 @@
+public class Doge {
+    public static void main(String[] args) {
+        String logo = " ____                   \n"
+                + "|  _ \\  ___   __ _  ___ \n"
+                + "| | | |/ _ \\ / _` |/ _ \\\n"
+                + "| |_| | (_) | (_| |  __/\n"
+                + "|____/ \\___/ \\__,_|\\___|\n"
+                + "             |___/        \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
