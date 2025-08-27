8e9dd450d14cd96d158331298dd119bb189b7685
brien <brienlim21@gmail.com>
2025-08-27 23:49:24 +0800

    Made Doge echo commands entered by the user

--- src/main/java/Doge.java
+++ src/main/java/Doge.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Doge {
     public static void main(String[] args) {
         String logo = " ____                   \n" +
@@ -8,10 +10,30 @@ public class Doge {
                 "             |___/      ";
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Doge\n" +
+                " Hello! Am Doge\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+
+        //creates a scanner object to read the input
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            //reads input
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        input + "\n" +
+                        "____________________________________________________________\n");
+            }
+        }
+
+        scanner.close();
     }
+
 }
