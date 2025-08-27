b6dd6e136900b17ce07dec88d3abb631856e4621
brien <brienlim21@gmail.com>
2025-08-28 00:16:19 +0800

    Doge now also stores text and lists them when requested

--- src/main/java/Doge.java
+++ src/main/java/Doge.java
@@ -17,19 +17,30 @@ public class Doge {
         //creates a scanner object to read the input
         Scanner scanner = new Scanner(System.in);
 
+        //array to store text input by the user
+        String[] textList = new String[100];
+        int numberOfTexts = 0;
+
         while(true) {
-            //reads input
-            String input = scanner.nextLine();
+            String input = scanner.nextLine(); //reads input
 
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________\n");
+                for (int i = 0; i < numberOfTexts; i++) {
+                    System.out.println((i + 1) + ". " + textList[i]);
+                }
+                System.out.println("____________________________________________________________\n");
             } else {
+                textList[numberOfTexts] = input;
+                numberOfTexts++;
                 System.out.println("____________________________________________________________\n" +
-                        input + "\n" +
-                        "____________________________________________________________\n");
+                        "added: " + input +
+                        "\n____________________________________________________________\n");
             }
         }
 
